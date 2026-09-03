bafba49874282ffca3af119a611fab5b09ff9294
Turadh-yy <2433505754@qq.com>
2026-09-03 17:41:39 +0800

    更新圆序台 2026/9/3

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - 安装时预缓存 shell
    - activate 清理旧版本缓存
 */
-var CACHE_VERSION = 'yuanxutai-v7';
+var CACHE_VERSION = 'yuanxutai-v8';
 var SHELL_CACHE = CACHE_VERSION + '-shell';
 var RUNTIME_CACHE = CACHE_VERSION + '-runtime';
 
